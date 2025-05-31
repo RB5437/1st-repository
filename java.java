f87ef8ad611ed5a96dcf3cbc6fab67633f41536e
RB5437 <Ritikbawane5437@gmail.com>
2025-05-31 14:27:41 +0530

    Update java.java

--- java.java
+++ java.java
@@ -7,4 +7,5 @@ SGID: sg-04430f5d7b1973a87
 
 aws ec2 run-instances --image-id ami-0554aa6767e249943--instance-type t2.micro  --key-name Key_Aws --security-group-ids  sg-04430f5d7b1973a87
     
-    
+aws ec2 run-instances --image-id ami-0554aa6767e249943--instance-type t2.micro  --key-name Key_Aws --security-group-ids  sg-04430f5d7b1973a88   
+aws ec2 run-instances --image-id ami-0554aa6767e249943--instance-type t2.micro  --key-name Key_Aws --security-group-ids  sg-04430f5d7b1973a899
